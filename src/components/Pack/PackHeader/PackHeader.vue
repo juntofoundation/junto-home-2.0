b222295c683b800500694b0921fd5c106d61e698
Eric Yang <eric@junto.love>
2019-06-27 15:40:30 -0400

    Build Pack Page

---
+++ src/components/Pack/PackHeader/PackHeader.vue
@@ -0,0 +1,156 @@
+
+<template>
+    <div class="packHeader">
+        <h2 class="packHeader__title">Mission</h2>
+        <p class="packHeader__subtitle"> We are a group of individuals who have come together from
+            many walks of life to help realign technology with humanity. We envision social technology
+            as a tool to activate our creative, collaborative, and expressive capacities and 
+            welcome anyone who feels called to participate to join this movement.
+        </p>
+
+        <div class="packHeader__section">
+            <h2 class="packHeader__title">Pack</h2>        
+            <div class="packHeader__pack">
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img src="../../../assets/images/junto-home-2.0__headshot--eric.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Eric Yang</p>
+                        <p class="packHeader__member--title">Founder, Executive Director</p>
+                    </div>
+                </div>
+
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img style="transform: translateY(-4rem);" src="../../../assets/images/junto-home-2.0__headshot--yaz.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Yaz Owainati</p>
+                        <p class="packHeader__member--title">Head of Outreach</p>
+                    </div>
+                </div>
+
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img src="../../../assets/images/junto-home-2.0__headshot--josh.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Josh Parkin</p>
+                        <p class="packHeader__member--title">CTO</p>
+                    </div>
+                </div>             
+
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img src="../../../assets/images/junto-home-2.0__headshot--drea.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Drea Bennett</p>
+                        <p class="packHeader__member--title">Creative Director</p>
+                    </div>
+                </div>        
+
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img src="../../../assets/images/junto-home-2.0__headshot--dora.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Dora Czovek</p>
+                        <p class="packHeader__member--title">Head of Operations</p>
+                    </div>
+                </div>                                             
+            </div>             
+        </div>        
+
+        <div class="packHeader__section">
+            <h2 class="packHeader__title">Board of Directors</h2>        
+            <div class="packHeader__pack">
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img src="../../../assets/images/junto-home-2.0__headshot--eric.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Eric Yang</p>
+                        <p class="packHeader__member--title">President, Board Member</p>
+                    </div>
+                </div>
+
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img src="../../../assets/images/junto-home-2.0__headshot--riley.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Riley Wagner</p>
+                        <p class="packHeader__member--title">Secretary, Board Member</p>
+                    </div>
+                </div>
+
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img src="../../../assets/images/junto-home-2.0__headshot--ekene.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Ekene Nkem-Mmekam</p>
+                        <p class="packHeader__member--title">Treasurer, Board Member</p>
+                    </div>
+                </div>             
+
+                <div class="packHeader__member">
+                    <div class="packHeader__member--photo">
+                        <img src="../../../assets/images/junto-home-2.0__headshot--kyle.png" alt="" class="packHeader__member--photo--image">
+                    </div>
+                    <div class="packHeader__member--details">
+                        <p class="packHeader__member--name">Kyle Hornick</p>
+                        <p class="packHeader__member--title">Board Member</p>
+                    </div>
+                </div>                              
+            </div>      
+        </div>
+
+        <h2 class="packHeader__title">Advisors</h2>        
+        <div class="packHeader__pack">
+            <div class="packHeader__member">
+                <div class="packHeader__member--photo">
+                    <img src="../../../assets/images/junto-home-2.0__headshot--art.png" alt="" class="packHeader__member--photo--image">
+                </div>
+                <div class="packHeader__member--details">
+                    <p class="packHeader__member--name">Arthur Brock</p>
+                    <p class="packHeader__member--title">Co-founder of Holochain</p>
+                </div>
+            </div>
+
+            <div class="packHeader__member">
+                <div class="packHeader__member--photo">
+                    <img src="../../../assets/images/junto-home-2.0__headshot--ray.png" alt="" class="packHeader__member--photo--image">
+                </div>
+                <div class="packHeader__member--details">
+                    <p class="packHeader__member--name">Raymond Powell</p>
+                    <p class="packHeader__member--title">Founder of CoGov</p>
+                </div>
+            </div>
+
+            <div class="packHeader__member">
+                <div class="packHeader__member--photo">
+                    <img src="../../../assets/images/junto-home-2.0__headshot--rushkoff.png" alt="" class="packHeader__member--photo--image">
+                </div>
+                <div class="packHeader__member--details">
+                    <p class="packHeader__member--name">Douglas Rushkoff</p>
+                    <p class="packHeader__member--title">Activist, Author</p>
+                </div>
+            </div>             
+
+            <div class="packHeader__member">
+                <div class="packHeader__member--photo">
+                    <img src="../../../assets/images/junto-home-2.0__headshot--dora.png" alt="" class="packHeader__member--photo--image">
+                </div>
+                <div class="packHeader__member--details">
+                    <p class="packHeader__member--name">Nick Manning</p>
+                    <p class="packHeader__member--title">Software Engineer</p>
+                </div>
+            </div>                              
+        </div>                    
+
+        
+    </div>
+</template>
--- src/pages/Pack/Pack.vue
+++ src/pages/Pack/Pack.vue
@@ -1,5 +1,33 @@
+
 <template>
     <div class="pack">
-        pack
+        <!-- navigation -->
+        <junto-nav></junto-nav>
+
+        <!-- pack header -->
+        <junto-pack-header></junto-pack-header>
+
+        <!-- flux-->
+        <junto-flux></junto-flux>      
+
+        <!-- footer-->
+        <junto-footer></junto-footer>                
+
     </div>
 </template>
+
+<script>
+    import juntoNav from '../../components/Nav/Nav.vue';
+    import juntoPackHeader from '../../components/Pack/PackHeader/PackHeader.vue';
+    import juntoFlux from '../../components/Flux/Flux.vue';
+    import juntoFooter from '../../components/Footer/Footer.vue';
+
+    export default {
+        components: {
+            juntoNav,
+            juntoPackHeader,
+            juntoFlux,
+            juntoFooter
+        }
+    }
+</script>
