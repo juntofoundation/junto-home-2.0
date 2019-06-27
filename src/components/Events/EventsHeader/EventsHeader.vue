826d7fdf2c98b9680704e38a88f401e6f4a4577e
Eric Yang <eric@junto.love>
2019-06-27 18:37:53 -0400

    Refactor Events

--- src/components/Events/EventsHeader/EventsHeader.vue
+++ src/components/Events/EventsHeader/EventsHeader.vue
@@ -1,12 +1,13 @@
 
 <template>
     <div class="eventsHeader">
-        <p class="eventsHeader__title">Events</p>
-        <p class="eventsHeader__subtitle">We host events every month!</p>
+        <p class="eventsHeader__title">Junto Events</p>
+        <p class="eventsHeader__subtitle">We understand social media can do so much. Our commmunity
+            holds space for a variety of physical events around the world. Come connect with people
+            and reach out if you'd like to host Junto events in your city!
+        </p>
         <div class="eventsHeader__calendar">
             <iframe class="eventsHeader__calendar--calendar" src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FNew_York&amp;src=YTdqNWpuOXZkazRpMXNuamZqNjR0OGlxN3NAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23AD1457&amp;showNav=1&amp;showDate=1&amp;showPrint=0&amp;showTabs=0&amp;showTitle=0" style="border-width:0" frameborder="0" scrolling="no"></iframe>
-
-
         </div>
 
     </div>
