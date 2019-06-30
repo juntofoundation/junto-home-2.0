15b655fb8f48f1d871cc4b20df9463635a80007f
Eric Yang <eric@junto.love>
2019-06-30 17:53:01 -0400

    Refactor LearnHeaderDesign and conditionally render

--- src/components/Learn/LearnHeader/LearnHeader.vue
+++ src/components/Learn/LearnHeader/LearnHeader.vue
@@ -7,105 +7,50 @@
             sustainable online and offline tools dedicated to activating the vast resovoir of human potential.
         </p>
         <div class="learnHeader__nav">
-            <p class="learnHeader__nav--item">DESIGN</p>
-            <p class="learnHeader__nav--item">TECHNOLOGY</p>
-            <p class="learnHeader__nav--item">NOT FOR PROFIT</p>
+            <p class="learnHeader__nav--item" @click="setCategory('design')">DESIGN</p>
+            <p class="learnHeader__nav--item" @click="setCategory('technology')">TECHNOLOGY</p>
+            <p class="learnHeader__nav--item" @click="setCategory('nonprofit')">NOT FOR PROFIT</p>
         </div>
-        <div class="learnHeader__section">
-            <div class="learnHeader__section--left">
-                <div class="learnHeader__section--body">
-                    <h2 class="learnHeader__section--title">Design Principles</h2>
-                    <p class="learnHeader__section--text">- Agency</p>
-                    <p class="learnHeader__section--text">- Accountability</p>
-                    <p class="learnHeader__section--text">- Authenticity</p>
-                    <p class="learnHeader__section--text">- Transparency</p>
-                    <p class="learnHeader__section--text">- Simplicity</p>                    
-
-                </div>
-            </div>
-
-            <div class="learnHeader__section--right">
-                <iframe class="learnHeader__section--video" src="https://www.youtube.com/embed/Ko6J9v1C9zE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                
-            </div>            
-
-        </div>
-
-        <div class="learnHeader__section">
-            <div class="learnHeader__section--left">
-                <img src="../../../assets/images/junto-home-2.0__learn--flows.png" class="learnHeader__section--visual" alt="">
-            </div>
-
-            <div class="learnHeader__section--right">
-                <h2 class="learnHeader__section--title">Conscious Social Flows</h2>
-                    <p class="learnHeader__section--text">- Transparent social profiles</p>
-                    <p class="learnHeader__section--text">- No like buttons or mandatory follower counts</p>
-                    <p class="learnHeader__section--text">- Junto is completely free of advertisements</p>
-                    <p class="learnHeader__section--text">- Fully customizeable notifications</p>
-             
-            </div>            
-
-        </div>       
-
-        <div class="learnHeader__section">
-            <div class="learnHeader__section--left">
-                <div class="learnHeader__section--body">
-                    <h2 class="learnHeader__section--title">Dynamic Creation</h2>
-                    <p class="learnHeader__section--text">- Create in an everexpanding number of ways</p>
-                    <p class="learnHeader__section--text">- Our initial release will include Longform, Photo, Shortform, Bullet, and Events</p>
-                    <p class="learnHeader__section--text">- These will evolve
-                        with the types of expressions the community wants to exist</p> 
-                    <p class="learnHeader__section--text">- This center of expression will be fully customizable</p>                         
-
-                </div>
-            </div>
-
-            <div class="learnHeader__section--right">
-                <img src="../../../assets/images/junto-home-2.0__learn--flows.png" class="learnHeader__section--visual" alt="">
-            </div>            
-
-        </div>      
-
-        <div class="learnHeader__section">
-            <div class="learnHeader__section--left">
-                <img src="../../../assets/images/junto-home-2.0__learn--flows.png" class="learnHeader__section--visual" alt="">
-            </div>
-
-            <div class="learnHeader__section--right">
-                <h2 class="learnHeader__section--title">Dynamic Discovery</h2>
-
-                    <p class="learnHeader__section--text">- Discover people and information through your social graph (degrees of separation)</p>
-                    <p class="learnHeader__section--text">- Create your own perspectives by adding individuals to unique feeds</p>
-                    <p class="learnHeader__section--text">- Explore any perspective by channels (topics)</p>
-                    <p class="learnHeader__section--text">- No algorithims that reinforce existing perspectives. You design your own experience.</p>
-             
-            </div>            
-
-        </div>      
-
-        <div class="learnHeader__section">
-            <div class="learnHeader__section--left">
-                <div class="learnHeader__section--body">
-                    <h2 class="learnHeader__section--title">Multiple Layers </h2>
-                    <p class="learnHeader__section--text">- Interact within the 'collective'    (public layer)</p>
-                    <p class="learnHeader__section--text">- Create and join specific 'spheres' (communities)</p>
-                    <p class="learnHeader__section--text">- Share and discover between your pack (semi-private layer)</p> 
-                    <p class="learnHeader__section--text">- Keep things to yourself in your den (private layer)</p>                         
-
-                </div>
-            </div>
-
-            <div class="learnHeader__section--right">
-                <img src="../../../assets/images/junto-home-2.0__learn--flows.png" class="learnHeader__section--visual" alt="">
-            </div>            
-
-        </div>             
-
+       
+        <junto-learn-header-design v-if="design"></junto-learn-header-design>
 
         <div class="learnHeader__section--reads">
-            
+
         </div>
         
                  
     </div>    
 </template>
 
+<script>
+import juntoLearnHeaderDesign from './LearnHeaderDesign/LearnHeaderDesign.vue';
+
+    export default {
+        data() {
+            return {
+                design: true,
+                technology: false,
+                nonprofit:false
+
+            }
+        },
+        methods: {
+            setCategory(category) {
+                this.design = false;
+                this.technology = false;
+                this.nonprofit = false;
+
+                if(category == 'design') {
+                    this.design = true;
+                } else if(category == 'technology') {
+                    this.technology = true;
+                } else if(category == 'nonprofit') {
+                    this.nonprofit = true;
+                }
+            }
+        },
+        components: {
+            juntoLearnHeaderDesign
+        }
+    }
+</script>
---
+++ src/components/Learn/LearnHeader/LearnHeaderDesign/LearnHeaderDesign.vue
@@ -0,0 +1,93 @@
+
+<template>
+    <div>
+        <div class="learnHeader__section">
+            <div class="learnHeader__section--left">
+                <div class="learnHeader__section--body">
+                    <h2 class="learnHeader__section--title">Design Principles</h2>
+                    <p class="learnHeader__section--text">- Agency</p>
+                    <p class="learnHeader__section--text">- Accountability</p>
+                    <p class="learnHeader__section--text">- Authenticity</p>
+                    <p class="learnHeader__section--text">- Transparency</p>
+                    <p class="learnHeader__section--text">- Simplicity</p>                    
+
+                </div>
+            </div>
+
+            <div class="learnHeader__section--right">
+                <iframe class="learnHeader__section--video" src="https://www.youtube.com/embed/Ko6J9v1C9zE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                
+            </div>            
+
+        </div>
+
+        <div class="learnHeader__section">
+            <div class="learnHeader__section--left">
+                <img src="../../../../assets/images/junto-home-2.0__learn--flows.png" class="learnHeader__section--visual" alt="">
+            </div>
+
+            <div class="learnHeader__section--right">
+                <h2 class="learnHeader__section--title">Conscious Social Flows</h2>
+                    <p class="learnHeader__section--text">- Transparent social profiles</p>
+                    <p class="learnHeader__section--text">- No like buttons or mandatory follower counts</p>
+                    <p class="learnHeader__section--text">- Junto is completely free of advertisements</p>
+                    <p class="learnHeader__section--text">- Fully customizeable notifications</p>
+             
+            </div>            
+
+        </div>       
+
+        <div class="learnHeader__section">
+            <div class="learnHeader__section--left">
+                <div class="learnHeader__section--body">
+                    <h2 class="learnHeader__section--title">Dynamic Creation</h2>
+                    <p class="learnHeader__section--text">- Create in an everexpanding number of ways</p>
+                    <p class="learnHeader__section--text">- Our initial release will include Longform, Photo, Shortform, Bullet, and Events</p>
+                    <p class="learnHeader__section--text">- These will evolve
+                        with the types of expressions the community wants to exist</p> 
+                    <p class="learnHeader__section--text">- This center of expression will be fully customizable</p>                         
+
+                </div>
+            </div>
+
+            <div class="learnHeader__section--right">
+                <img src="../../../../assets/images/junto-home-2.0__learn--flows.png" class="learnHeader__section--visual" alt="">
+            </div>            
+
+        </div>      
+
+        <div class="learnHeader__section">
+            <div class="learnHeader__section--left">
+                <img src="../../../../assets/images/junto-home-2.0__learn--flows.png" class="learnHeader__section--visual" alt="">
+            </div>
+
+            <div class="learnHeader__section--right">
+                <h2 class="learnHeader__section--title">Dynamic Discovery</h2>
+
+                    <p class="learnHeader__section--text">- Discover people and information through your social graph (degrees of separation)</p>
+                    <p class="learnHeader__section--text">- Create your own perspectives by adding individuals to unique feeds</p>
+                    <p class="learnHeader__section--text">- Explore any perspective by channels (topics)</p>
+                    <p class="learnHeader__section--text">- No algorithims that reinforce existing perspectives. You design your own experience.</p>
+             
+            </div>            
+
+        </div>      
+
+        <div class="learnHeader__section">
+            <div class="learnHeader__section--left">
+                <div class="learnHeader__section--body">
+                    <h2 class="learnHeader__section--title">Multiple Layers </h2>
+                    <p class="learnHeader__section--text">- Interact within the 'collective'    (public layer)</p>
+                    <p class="learnHeader__section--text">- Create and join specific 'spheres' (communities)</p>
+                    <p class="learnHeader__section--text">- Share and discover between your pack (semi-private layer)</p> 
+                    <p class="learnHeader__section--text">- Keep things to yourself in your den (private layer)</p>                         
+
+                </div>
+            </div>
+
+            <div class="learnHeader__section--right">
+                <img src="../../../../assets/images/junto-home-2.0__learn--flows.png" class="learnHeader__section--visual" alt="">
+            </div>            
+
+        </div>              
+    </div>
+</template>
