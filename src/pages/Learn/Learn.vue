1b328233eb1ed61903ee63b56825f93423927bf9
Eric Yang <eric@junto.love>
2019-07-05 18:59:10 -0400

    Add nav mobile to all pages

--- src/pages/Learn/Learn.vue
+++ src/pages/Learn/Learn.vue
@@ -2,7 +2,21 @@
 <template>
     <div class="learn">
         <!-- navigation -->
-        <junto-nav></junto-nav>
+        <junto-nav>
+            <div slot="navIcon" class="nav__icon" @click="navOpen = true">
+                <a class="removelink">
+                    <img src="../../assets/images/junto-home-2.0__moon.png" alt="" class="nav__icon--image">
+                </a>
+            </div>                    
+        </junto-nav>
+
+        <!-- navigation -->
+        <transition name="fade"> 
+            <junto-nav-open v-if="navOpen">
+                <p @click="navOpen = false" slot="navClose" class="navOpen__nav--item">CLOSE</p>
+            
+            </junto-nav-open>        
+        </transition>
 
         <!-- learn header -->
         <junto-learn-header></junto-learn-header>
@@ -18,16 +32,37 @@
 
 <script>
     import juntoNav from '../../components/Nav/Nav.vue';
+    import juntoNavOpen from '../../components/NavOpen/NavOpen.vue';
     import juntoFlux from '../../components/Flux/Flux.vue';
     import juntoLearnHeader from '../../components/Learn/LearnHeader/LearnHeader.vue';
     import juntoFooter from '../../components/Footer/Footer.vue';
 
     export default {
+        data() {
+            return {
+                navOpen: false  
+            }
+        },
+
         components: {
             juntoNav,
+            juntoNavOpen,
             juntoFlux,
             juntoLearnHeader,
             juntoFooter
         }
     }
 </script>
+
+
+<style>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .2s
+    }
+
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0
+    }
+</style>
--- src/pages/Pack/Pack.vue
+++ src/pages/Pack/Pack.vue
@@ -2,7 +2,21 @@
 <template>
     <div class="pack">
         <!-- navigation -->
-        <junto-nav></junto-nav>
+        <junto-nav>
+            <div slot="navIcon" class="nav__icon" @click="navOpen = true">
+                <a class="removelink">
+                    <img src="../../assets/images/junto-home-2.0__moon.png" alt="" class="nav__icon--image">
+                </a>
+            </div>                    
+        </junto-nav>
+
+        <!-- navigation -->
+        <transition name="fade"> 
+            <junto-nav-open v-if="navOpen">
+                <p @click="navOpen = false" slot="navClose" class="navOpen__nav--item">CLOSE</p>
+            
+            </junto-nav-open>        
+        </transition>
 
         <!-- pack header -->
         <junto-pack-header></junto-pack-header>
@@ -18,16 +32,36 @@
 
 <script>
     import juntoNav from '../../components/Nav/Nav.vue';
+    import juntoNavOpen from '../../components/NavOpen/NavOpen.vue';
     import juntoPackHeader from '../../components/Pack/PackHeader/PackHeader.vue';
     import juntoFlux from '../../components/Flux/Flux.vue';
     import juntoFooter from '../../components/Footer/Footer.vue';
 
     export default {
+        data() {
+            return {
+                navOpen: false  
+            }
+        },        
         components: {
             juntoNav,
+            juntoNavOpen,
             juntoPackHeader,
             juntoFlux,
             juntoFooter
         }
     }
 </script>
+
+
+<style>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .2s
+    }
+
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0
+    }
+</style>
