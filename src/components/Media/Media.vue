6ceac8545ed57ce38cf0a99731b11dd6c388dc88
Eric Yang <eric@junto.love>
2019-07-06 17:10:18 -0400

    Make media library responsive

--- src/components/Media/Media.vue
+++ src/components/Media/Media.vue
@@ -6,21 +6,21 @@
                 <p class="media__logo--name">JUNTO VIDEO LIBRARY</p>
             </div>
             <slot name="back"></slot>
-        </div>                
+        </div>                 
 
         <div class="media__bottom">
-            <div class="media__main">
-                
+            <div class="media__main">                
                 <iframe v-if="videoOne" class="media__main--video" src="https://www.youtube.com/embed/HsWSHH7hHRI" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <iframe v-if="videoTwo" class="media__main--video" src="https://www.youtube.com/embed/tsMeJrVQis8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                                
                 <iframe v-if="videoThree" class="media__main--video" src="https://player.vimeo.com/video/341139447" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>                
                 <iframe v-if="videoFour" class="media__main--video" src="https://www.youtube.com/embed/k7a_c20vaC8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                            
                 <iframe v-if="videoFive" class="media__main--video" src="https://www.youtube.com/embed/yaZCqaJw8yc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>            
+                <iframe v-if="videoSix" class="media__main--video" src="https://www.youtube.com/embed/Ge_k74pwJn8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                
             </div>
             <div class="media__videos">
                 <p class="media__videos--title">PLAYLIST</p>
                 <div class="media__video" @click="updateVideo('one')">
-                    <img class="media__video--thumbnail" src="../../assets/images/junto-home-2.0__thumbnail--flows.png" alt="">
+                    <div class="media__video--thumbnail media__video--thumbnail--one">&nbsp;</div>
                     <div class="media__video--details">
                         <p class="media__video--title">Expression that Flows</p>
                         <p class="media__video--date">Jan 26, 2019</p>
@@ -28,7 +28,7 @@
                 </div>
 
                 <div class="media__video" @click="updateVideo('two')">
-                    <img class="media__video--thumbnail" src="../../assets/images/junto-home-2.0__thumbnail--hcxjunto.png" alt="">
+                    <div class="media__video--thumbnail media__video--thumbnail--two">&nbsp;</div>
                     <div class="media__video--details">
                         <p class="media__video--title">Powered by Holochain Interview Series: Junto</p>
                         <p class="media__video--date">Jan 27, 2019</p>
@@ -36,7 +36,7 @@
                 </div>   
 
                 <div class="media__video" @click="updateVideo('three')">
-                    <img class="media__video--thumbnail" src="../../assets/images/junto-home-2.0__thumbnail--demo.png" alt="">
+                    <div class="media__video--thumbnail media__video--thumbnail--three">&nbsp;</div>
                     <div class="media__video--details">
                         <p class="media__video--title">Junto Demo #1</p>
                         <p class="media__video--date">Jun 8, 2019</p>
@@ -44,7 +44,7 @@
                     </div>                             
 
                 <div class="media__video" @click="updateVideo('four')">
-                    <img class="media__video--thumbnail" src="../../assets/images/junto-home-2.0__thumbnail--decentralization.png" alt="">
+                    <div class="media__video--thumbnail media__video--thumbnail--four">&nbsp;</div>
                     <div class="media__video--details">
                         <p class="media__video--title">Why Does Decentralization Matter?</p>
                         <p class="media__video--date">Mar 11, 2019</p>
@@ -52,18 +52,18 @@
                 </div>   
 
                 <div class="media__video" @click="updateVideo('five')">
-                    <img class="media__video--thumbnail" src="../../assets/images/junto-home-2.0__thumbnail--communitysm.png" alt="">
+                    <div class="media__video--thumbnail media__video--thumbnail--five">&nbsp;</div>
                     <div class="media__video--details">
                         <p class="media__video--title">Junto Community Series - How Do You Feel About Social Media?</p>
                         <p class="media__video--date">Feb 7, 2019</p>
                     </div>                        
                 </div>     
 
-                <div class="media__video" @click="updateVideo('four')">
-                    <img class="media__video--thumbnail" src="../../assets/images/junto-home-2.0__thumbnail--decentralization.png" alt="">
+                <div class="media__video" @click="updateVideo('six')">
+                    <div class="media__video--thumbnail media__video--thumbnail--six">&nbsp;</div>
                     <div class="media__video--details">
-                        <p class="media__video--title">Why Does Decentralization Matter?</p>
-                        <p class="media__video--date">Mar 11, 2019</p>
+                        <p class="media__video--title">Junto Outtakes</p>
+                        <p class="media__video--date">Jul 3, 2019</p>
                     </div>                       
                 </div>                                                                                                              
             </div>                    
@@ -92,6 +92,7 @@
                  this.videoThree = false;
                  this.videoFour = false;
                  this.videoFive = false;
+                 this.videoSix = false;
 
                 if(video == 'one') {
                     this.videoOne = true;
@@ -103,6 +104,8 @@
                     this.videoFour = true;
                 } else if (video =='five') {
                     this.videoFive = true;
+                } else if (video =='six') {
+                    this.videoSix = true;
                 }
              }
          }
