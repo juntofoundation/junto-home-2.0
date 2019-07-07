3f124c0310e2bdb0cb6eac29c3c3540e8dd77a68
Eric Yang <eric@junto.love>
2019-07-06 21:51:00 -0400

    Add mobile menu to PackMember

--- src/pages/Pack/PackMembers/PackDrea.vue
+++ src/pages/Pack/PackMembers/PackDrea.vue
@@ -2,7 +2,20 @@
 <template>
     <div class="pack">
         <!-- navigation -->
-        <junto-nav></junto-nav>
+        <junto-nav>
+            <div slot="navIcon" class="nav__icon" @click="navOpen = true">
+                <a class="removelink">
+                    <img src="../../../assets/images/junto-home-2.0__moon.png" alt="" class="nav__icon--image">
+                </a>
+            </div>                    
+        </junto-nav>
+
+        <!-- navigation -->
+        <transition name="fade"> 
+            <junto-nav-open v-if="navOpen">
+                <img @click="navOpen = false" slot="navClose" class="navOpen__nav--icon" src="../../../assets/images/junto-home-2.0__moon--filled.png">                        
+            </junto-nav-open>        
+        </transition>
 
         <div class="packMember">
                 <div class="packMember__left">
@@ -31,12 +44,31 @@
 
 <script>
     import juntoNav from '../../../components/Nav/Nav.vue';
+    import juntoNavOpen from '../../../components/NavOpen/NavOpen.vue';
     import juntoFooter from '../../../components/Footer/Footer.vue';
 
     export default {
+        data() {
+            return {
+                navOpen: false  
+            }
+        },          
         components: {
             juntoNav,
+            juntoNavOpen,
             juntoFooter
         }
     }
 </script>
+
+<style>
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity .2s
+    }
+
+    .fade-enter,
+    .fade-leave-to {
+        opacity: 0
+    }
+</style>
