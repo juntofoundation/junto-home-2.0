c41e41c5927e4f0bc3207cbb44b9740a3fbff3a0
Eric Yang <eric@junto.love>
2020-05-18 17:39:22 -0500

    Update footer and learn header

--- src/components/Learn/LearnHeader/LearnHeader.vue
+++ src/components/Learn/LearnHeader/LearnHeader.vue
@@ -1,79 +1,104 @@
 
 <template>
-    <div class="learnHeader">
-        <h2 class="learnHeader__title">Rebalancing Our Relationship With Technology</h2>
-        <p class="learnHeader__subtitle"> 
-            Junto holistically integrates new patterns of design, technology, and governance to create
-            sustainable online and offline tools that embrace our human potential. 
-        </p>
-        <div class="learnHeader__nav">
-            <div class="learnHeader__nav--items">
-                <p class="learnHeader__nav--item" :class="{ active: design }" @click="setCategory('design')">DESIGN</p>
-                <p class="learnHeader__nav--item" :class="{ active: technology }" @click="setCategory('technology')">TECHNOLOGY</p>
-                <p class="learnHeader__nav--item" :class="{ active: nonprofit }" @click="setCategory('nonprofit')">NOT FOR PROFIT</p>                
-            </div>
-            <div class="learnHeader__nav--border">&nbsp;</div>
+  <div class="learnHeader">
+    <div class="learnHeader__header">
+      <h2 class="learnHeader__header--title">Learn About Our Holistic Approach</h2>
+    </div>
+    <div class="learnHeader__main">
+      <h2 class="learnHeader__main--title">Rebalancing our relationship with technology</h2>
+      <p class="learnHeader__main--subtitle">
+        Junto holistically integrates new patterns of design, technology, and governance to create
+        sustainable online and offline tools that embrace our human potential.
+      </p>
+      <div class="learnHeader__nav">
+        <div class="learnHeader__nav--items">
+          <p
+            class="learnHeader__nav--item"
+            :class="{ active: design }"
+            @click="setCategory('design')"
+          >DESIGN</p>
+          <p
+            class="learnHeader__nav--item"
+            :class="{ active: technology }"
+            @click="setCategory('technology')"
+          >TECHNOLOGY</p>
+          <p
+            class="learnHeader__nav--item"
+            :class="{ active: nonprofit }"
+            @click="setCategory('nonprofit')"
+          >NOT FOR PROFIT</p>
         </div>
-       
-        <junto-learn-header-design v-if="design">            
-            <p slot="learnDesignMore" @click="setCategory('technology')" class="learnHeader__more">Learn more about our technology &rarr;</p>
-        </junto-learn-header-design>
+        <div class="learnHeader__nav--border">&nbsp;</div>
+      </div>
 
-        <junto-learn-header-technology v-if="technology">
-            <p slot="learnGovernanceMore" @click="setCategory('nonprofit')" class="learnHeader__more">Learn more about our governance &rarr;</p>
-        </junto-learn-header-technology>
+      <junto-learn-header-design v-if="design">
+        <p
+          slot="learnDesignMore"
+          @click="setCategory('technology')"
+          class="learnHeader__more"
+        >Learn more about our technology &rarr;</p>
+      </junto-learn-header-design>
 
-        <junto-learn-header-nonprofit v-if="nonprofit">
-            <p slot="learnDesignMore" @click="setCategory('design')" class="learnHeader__more">Learn more about our design &rarr;</p>
-        </junto-learn-header-nonprofit>
+      <junto-learn-header-technology v-if="technology">
+        <p
+          slot="learnGovernanceMore"
+          @click="setCategory('nonprofit')"
+          class="learnHeader__more"
+        >Learn more about our governance &rarr;</p>
+      </junto-learn-header-technology>
 
-    </div>    
+      <junto-learn-header-nonprofit v-if="nonprofit">
+        <p
+          slot="learnDesignMore"
+          @click="setCategory('design')"
+          class="learnHeader__more"
+        >Learn more about our design &rarr;</p>
+      </junto-learn-header-nonprofit>
+    </div>
+  </div>
 </template>
 
 <script>
-    import juntoLearnHeaderDesign from './LearnHeaderDesign/LearnHeaderDesign.vue';
-    import juntoLearnHeaderTechnology from './LearnHeaderTechnology/LearnHeaderTechnology.vue';
-    import juntoLearnHeaderNonprofit from './LearnHeaderNonprofit/LearnHeaderNonprofit.vue';
+import juntoLearnHeaderDesign from "./LearnHeaderDesign/LearnHeaderDesign.vue";
+import juntoLearnHeaderTechnology from "./LearnHeaderTechnology/LearnHeaderTechnology.vue";
+import juntoLearnHeaderNonprofit from "./LearnHeaderNonprofit/LearnHeaderNonprofit.vue";
 
-    export default {
-        data() {
-            return {
-                design: true,
-                technology: false,
-                nonprofit:false
+export default {
+  data() {
+    return {
+      design: true,
+      technology: false,
+      nonprofit: false
+    };
+  },
+  methods: {
+    setCategory(category) {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
 
-            }
-        },
-        methods: {
-            setCategory(category) {
-                document.body.scrollTop = 0;
-                document.documentElement.scrollTop = 0;
+      this.design = false;
+      this.technology = false;
+      this.nonprofit = false;
 
-                this.design = false;
-                this.technology = false;
-                this.nonprofit = false;
-
-                if(category == 'design') {
-                    this.design = true;
-                } else if(category == 'technology') {
-                    this.technology = true;
-                } else if(category == 'nonprofit') {
-                    this.nonprofit = true;
-                }
-            }
-        },
-        components: {
-            juntoLearnHeaderDesign,
-            juntoLearnHeaderTechnology,
-            juntoLearnHeaderNonprofit,
-
-
-        }
+      if (category == "design") {
+        this.design = true;
+      } else if (category == "technology") {
+        this.technology = true;
+      } else if (category == "nonprofit") {
+        this.nonprofit = true;
+      }
     }
+  },
+  components: {
+    juntoLearnHeaderDesign,
+    juntoLearnHeaderTechnology,
+    juntoLearnHeaderNonprofit
+  }
+};
 </script>
 
 <style>
-    .active {
-        color: #333; 
-    }
+.active {
+  color: #333;
+}
 </style>
