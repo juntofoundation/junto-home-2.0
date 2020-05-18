3de1fb717e4c0373704b787237e84f65996c73e7
Eric Yang <eric@junto.love>
2020-05-18 14:41:19 -0500

    Create privacy policy page

---
+++ src/pages/PrivacyPolicy/PrivacyPolicy.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="privacyPolicy">
+    <!-- navigation -->
+    <junto-nav>
+      <div slot="navIcon" class="nav__icon" @click="navOpen = true">
+        <a class="removelink">
+          <img src="../../assets/images/junto-home-2.0__moon.png" alt class="nav__icon--image" />
+        </a>
+      </div>
+    </junto-nav>
+
+    <!-- navigation -->
+    <transition name="fade">
+      <junto-nav-open v-if="navOpen">
+        <img
+          @click="navOpen = false"
+          slot="navClose"
+          class="navOpen__nav--icon"
+          src="../../assets/images/junto-home-2.0__moon--filled.png"
+        />
+      </junto-nav-open>
+    </transition>
+
+    <!-- privacy policy header -->
+    <junto-privacy-policy-header></junto-privacy-policy-header>
+
+    <!-- footer-->
+    <junto-footer></junto-footer>
+  </div>
+</template>
+
+
+<script>
+import juntoNav from "../../components/Nav/Nav.vue";
+import juntoNavOpen from "../../components/NavOpen/NavOpen.vue";
+import juntoPrivacyPolicyHeader from "../../components/PrivacyPolicyHeader/PrivacyPolicyHeader.vue";
+import juntoFooter from "../../components/Footer/Footer.vue";
+
+export default {
+  data() {
+    return {
+      navOpen: false
+    };
+  },
+  components: {
+    juntoNav,
+    juntoNavOpen,
+    juntoPrivacyPolicyHeader,
+    juntoFooter
+  }
+};
+</script>
+
+<style>
+</style>
