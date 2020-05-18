e2f4a18117d26857ed00f59b101eef7f817fc915
Eric Yang <eric@junto.love>
2020-05-18 16:39:32 -0500

    Set up TOS template

---
+++ src/components/TermsOfServiceHeader/TermsOfServiceHeader.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div>
+    <div class="termsOfServiceHeader" v-if="memberOpen != true">
+      <!--  header + background image -->
+      <div class="termsOfServiceHeader__header">
+        <h2 class="termsOfServiceHeader__header--title">Junto Terms of Service</h2>
+        <p
+          class="termsOfServiceHeader__header--subtitle"
+        >We encourage you to read this policy in full.</p>
+      </div>
+    </div>
+  </div>
+</template>
+
