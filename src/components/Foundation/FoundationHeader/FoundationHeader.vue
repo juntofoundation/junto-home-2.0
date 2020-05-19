87ec9d86d9a2d539d9a7bd01c8154afdda84c299
Eric Yang <eric@junto.love>
2020-05-18 20:53:13 -0500

    Redesign foundation page

--- src/components/Foundation/FoundationHeader/FoundationHeader.vue
+++ src/components/Foundation/FoundationHeader/FoundationHeader.vue
@@ -1,90 +1,68 @@
 
 <template>
-    <div class="foundationHeader">
-        <h2 class="foundationHeader__title">Support the Junto Foundation</h2>
-        <p class="foundationHeader__subtitle"> Junto is a nonprofit organization registered
-            in New York. This is to ensure the independence and integrity of this organization is sustainable long term. 
-            Any contribution to help move Junto forward is much appreciated!            
-        </p>
-
-         <div class="foundationHeader__pillars">    
-             <div class="foundationHeader__pillar">
-                 <div class="foundationHeader__pillar--left foundationHeader__pillar--left--founder">&nbsp;</div>
-                 <div class="foundationHeader__pillar--right">
-                     <p class="foundationHeader__pillar--title">Grassroots Donations</p>
-                     <p class="foundationHeader__pillar--body">Everything we do is not for profit and open source. Your generosity and support is what will
-                         carry this movement forward. We invite all ranges of donations and walks of life to contribute - thank you for believing in this vision! 
-                     </p>
-                     <a target="blank" class="removelink">
-
-                        <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CX87U4QQQX2TW&source=url" target="blank" class="removelink"><button class="foundationHeader__pillar--button">Support Junto</button></a>
-                     </a>
-           
-                 </div>
-             </div>
-
-             <div class="foundationHeader__pillar">
-                 <div class="foundationHeader__pillar--left foundationHeader__pillar--left--sponsor">&nbsp;</div>
-                 <div class="foundationHeader__pillar--right">
-                     <p class="foundationHeader__pillar--title">Become A Sponsor/Philanthropist</p>
-                     <p class="foundationHeader__pillar--body">If you would like to sponsor Junto and be highlighted on our home page, send an email to eric@junto.foundation!  
-                         </p>
-                     <a href="mailto:eric@junto.foundation" target="blank" class="removelink"><button class="foundationHeader__pillar--button">Email Us!</button></a>
-                 </div>
-             </div>            
-             
-              
-<!-- 
-
-            <div class="foundationHeader__pillar">
-                <p class="foundationHeader__pillar--title">ONE-TIME DONATIONS</p>
-                <p class="foundationHeader__pillar--body">We accept donations through 
-                    <a href="https://www.indiegogo.com/projects/junto-a-new-breed-of-social-media--2" target="blank" class="addlink">Indiegogo</a> and Holo Tokens. 
-                    View our $HOT address below.
-                </p>                                        
-            </div>
-
-            <div class="foundationHeader__pillar foundationHeader__pillar--middle">
-                <p class="foundationHeader__pillar--title">BECOME A SPONSOR</p>
-                <p class="foundationHeader__pillar--body">If you're an individual or organization that 
-                    would like to sponsor Junto and be highlighted on our home page, send us an email <a href="mailto:hi@junto.foundation" class="addlink">here!</a> 
-                </p>            
-            </div>   -->
-
-
-            <!-- <div class="foundationHeader__pillar foundationHeader__pillar--middle">
-                <p class="foundationHeader__pillar--title">JUNTO MERCH</p>
-                <p class="foundationHeader__pillar--body">We are opening up a Junto merch store soon. All proceeds 
-                    go towards moving this initiative forward. Stay tuned!
-                </p>            
-            </div>                                  -->
-        </div> 
-
-        <!-- <div class="foundationHeader__hot">
-            <img src="../../../assets/images/junto-home-2.0__holo.jpg" alt="" class="foundationHeader__hot--icon">
-            <p class="foundationHeader__hot--title">Holo Token Address:</p>
-            <p class="foundationHeader__hot--address">0xCD75b4046A99101a232bB105915F182C7b7E136E</p>
-        </div> -->
-
+  <div class="foundationHeader">
+    <div class="packHeader__header">
+      <h2 class="packHeader__header--title">Not For Profit</h2>
     </div>
+    <div class="foundationHeader__main">
+      <h2 class="foundationHeader__main--title">Support the Junto Foundation</h2>
+      <p class="foundationHeader__main--subtitle">
+        Junto is a nonprofit organization registered
+        in New York. This is to ensure the independence and integrity of this organization is sustainable long term.
+        Any contribution to help move Junto forward is much appreciated!
+      </p>
+
+      <div class="foundationHeader__pillars">
+        <div class="foundationHeader__pillar">
+          <div class="foundationHeader__pillar--left foundationHeader__pillar--left--founder">&nbsp;</div>
+          <div class="foundationHeader__pillar--right">
+            <p class="foundationHeader__pillar--title">Grassroots Donations</p>
+            <p class="foundationHeader__pillar--body">
+              Everything we do is not for profit and open source. Your generosity and support is what will
+              carry this movement forward. We invite all ranges of donations and walks of life to contribute - thank you for believing in this vision!
+            </p>
+            <a target="blank" class="removelink">
+              <a
+                href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CX87U4QQQX2TW&source=url"
+                target="blank"
+                class="removelink"
+              >
+                <button class="foundationHeader__pillar--button">Support Junto</button>
+              </a>
+            </a>
+          </div>
+        </div>
+
+        <div class="foundationHeader__pillar">
+          <div class="foundationHeader__pillar--left foundationHeader__pillar--left--sponsor">&nbsp;</div>
+          <div class="foundationHeader__pillar--right">
+            <p class="foundationHeader__pillar--title">Become A Sponsor/Philanthropist</p>
+            <p
+              class="foundationHeader__pillar--body"
+            >If you would like to sponsor Junto and be highlighted on our home page, please end an email to eric@junto.foundation.</p>
+            <a href="mailto:eric@junto.foundation" target="blank" class="removelink">
+              <button class="foundationHeader__pillar--button">Get In Touch </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
-    export default {
-        data() {
-            return {
-                hotOn: false                    
-            }
-        },
+export default {
+  data() {
+    return {
+      hotOn: false
+    };
+  },
 
-        methods: {
-            showAddress() {
-
-            }
-        }
-    }
-
+  methods: {
+    showAddress() {}
+  }
+};
 </script>
 
 
