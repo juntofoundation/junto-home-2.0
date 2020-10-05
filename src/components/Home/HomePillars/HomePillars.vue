e46d65538d040e7e4549246979d835adf8e70f60
Eric Yang <eric@junto.foundation>
2020-10-05 00:07:58 -0500

    Fix typo + update nonprofit text

--- src/components/Home/HomePillars/HomePillars.vue
+++ src/components/Home/HomePillars/HomePillars.vue
@@ -1,45 +1,58 @@
 <template>
-    <div class="homePillars">
-        <h2 class="homePillars__title">META</h2>
-        <p class="homePillars__subtitle">The evolution of social media begins with
-            redesigning the fundamentals.
+  <div class="homePillars">
+    <h2 class="homePillars__title">META</h2>
+    <p class="homePillars__subtitle">
+      The evolution of social media begins with redesigning the fundamentals.
+    </p>
+    <div class="homePillars__pillars">
+      <div class="homePillars__pillar">
+        <img
+          src="../../../assets/images/junto-home-2.0__pillar--design.png"
+          alt=""
+          class="homePillars__pillar--icon"
+        />
+        <p class="homePillars__pillar--title">HUMAN-CENTERED DESIGN</p>
+        <p class="homePillars__pillar--body">
+          The forms we communicate through are as important as the messages
+          themselves. We've designed new patterns to shape a digital culture
+          that inspires authenticity and connection. No ads, AI-powered echo
+          chambers, and more.
         </p>
-        <div class="homePillars__pillars">
-            <div class="homePillars__pillar">
-                <img src="../../../assets/images/junto-home-2.0__pillar--design.png" alt="" class="homePillars__pillar--icon">
-                <p class="homePillars__pillar--title">HUMAN-CENTERED DESIGN</p>
-                <p class="homePillars__pillar--body">The forms we communicate through
-                    are as important as the messages themselves. We've designed new patterns to
-                    shape a digital culture that inspires authenticity and connection. 
-                    No ads, AI-powered echo chambers, and more. 
-                </p>
-                
-            </div>
+      </div>
 
-            <div class="homePillars__pillar homePillars__pillar--middle">
-                <img src="../../../assets/images/junto-home-2.0__pillar--decentralized.png" alt="" class="homePillars__pillar--icon">
-                <p class="homePillars__pillar--title">DISTRIBUTED TECHNOLOGY</p>
-                <p class="homePillars__pillar--body">Junto will be powered by a scalable, distributed
-                    technology called Holochain. This means we don't store your information in
-                    centralized servers run or owned by us. You own your data and we won't
-                    sell it to third parties. Our initilal release will be centralized, but open source.
-                </p> 
-                
+      <div class="homePillars__pillar homePillars__pillar--middle">
+        <img
+          src="../../../assets/images/junto-home-2.0__pillar--decentralized.png"
+          alt=""
+          class="homePillars__pillar--icon"
+        />
+        <p class="homePillars__pillar--title">DISTRIBUTED TECHNOLOGY</p>
+        <p class="homePillars__pillar--body">
+          Junto will be powered by a scalable, distributed technology called
+          Holochain. This means we don't store your information in centralized
+          servers run or owned by us. You own your data and we won't sell it to
+          third parties. Our initial release will be centralized, but open
+          source.
+        </p>
+      </div>
 
-            </div>
-
-            <div class="homePillars__pillar">
-                <img src="../../../assets/images/junto-home-2.0__pillar--nonprofit.png" alt="" class="homePillars__pillar--icon">
-                <p class="homePillars__pillar--title">NOT FOR PROFIT</p>
-                <p class="homePillars__pillar--body">Junto is a nonprofit organization
-                    registered in New York. It exists independently of any  
-                    shareholders or special interests. This movement
-                    belongs to all of us and none of us. 
-                </p>
-            </div>                        
-        </div>
-        <a href="/learn" class="removelink">
-            <button class="homePillars__more">GO IN DEPTH</button>
-        </a>
+      <div class="homePillars__pillar">
+        <img
+          src="../../../assets/images/junto-home-2.0__pillar--nonprofit.png"
+          alt=""
+          class="homePillars__pillar--icon"
+        />
+        <p class="homePillars__pillar--title">NOT FOR PROFIT</p>
+        <p class="homePillars__pillar--body">
+          Junto is a nonprofit organization independent of any shareholders or
+          special interests. We are building tools for you to shape its
+          direction in a decentralized manner. This movement belongs to all of
+          us and none of us.
+        </p>
+      </div>
     </div>
+    <a href="/learn" class="removelink">
+      <button class="homePillars__more">GO IN DEPTH</button>
+    </a>
+  </div>
 </template>
