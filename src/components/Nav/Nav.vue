e2b71b62ca8e3ae9f22eaf734f54aad4f16487f2
Eric Yang <eric@junto.foundation>
2020-10-05 00:16:23 -0500

    Update typeform

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,42 +1,55 @@
 
 <template>
-    <div class="nav">
-        <a href="/" class="removelink">
-            <div class="nav__logo--container">
-                <img class="nav__logo--image" src="../../assets/images/junto-home-2.0__logo--rainbow.png" alt="">
-            </div>        
-        </a>
-        <div class="nav__items"> 
-            <div class="nav__item">
-                <a href='/learn' class="removelink">LEARN</a>
-            </div>        
-                                              
-            <div class="nav__item">
-                <a href='/pack' class="removelink">PACK</a>
-            </div>
-            <div class="nav__item">
-                <a href='/foundation' class="removelink">FOUNDATION</a>
-            </div>            
-            <div class="nav__item">
-                <a href='/events' class="removelink">EVENTS</a>
-            </div>
-            <!-- <div class="nav__item">
+  <div class="nav">
+    <a href="/" class="removelink">
+      <div class="nav__logo--container">
+        <img
+          class="nav__logo--image"
+          src="../../assets/images/junto-home-2.0__logo--rainbow.png"
+          alt=""
+        />
+      </div>
+    </a>
+    <div class="nav__items">
+      <div class="nav__item">
+        <a href="/learn" class="removelink">LEARN</a>
+      </div>
+
+      <div class="nav__item">
+        <a href="/pack" class="removelink">PACK</a>
+      </div>
+      <div class="nav__item">
+        <a href="/foundation" class="removelink">FOUNDATION</a>
+      </div>
+      <div class="nav__item">
+        <a href="/events" class="removelink">EVENTS</a>
+      </div>
+      <!-- <div class="nav__item">
                 <a href='/manifesto' class="removelink">MANIFESTO</a>
             </div> -->
-            <div class="nav__item">
-                <a href='/faq' class="removelink">FAQ</a>
-            </div>   
+      <div class="nav__item">
+        <a href="/faq" class="removelink">FAQ</a>
+      </div>
 
-            <div class="nav__button--outline">
-                <a href='https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CX87U4QQQX2TW&source=url' target="blank" class="removelink">DONATE</a>
-            </div>                 
+      <div class="nav__button--outline">
+        <a
+          href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=CX87U4QQQX2TW&source=url"
+          target="blank"
+          class="removelink"
+          >DONATE</a
+        >
+      </div>
 
-            <div class="nav__button">
-                <a href='https://junto.typeform.com/to/xpwCxK' target="blank" class="removelink">SIGN UP</a>
-            </div>     
+      <div class="nav__button">
+        <a
+          href="https://junto.typeform.com/to/iY66XCod"
+          target="blank"
+          class="removelink"
+          >SIGN UP</a
+        >
+      </div>
 
-            <slot name="navIcon"></slot>               
-        </div>
-
+      <slot name="navIcon"></slot>
     </div>
+  </div>
 </template>
