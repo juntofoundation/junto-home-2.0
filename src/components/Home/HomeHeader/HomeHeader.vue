917cc7a248ae60ed98121c710f532e7dfc883a84
Eric Yang <eric@junto.foundation>
2022-01-30 16:15:25 -0600

    update home header

--- src/components/Home/HomeHeader/HomeHeader.vue
+++ src/components/Home/HomeHeader/HomeHeader.vue
@@ -2,19 +2,18 @@
     <div class="homeHeader">
         <!-- backdrop -->
             <div class="homeHeader__content">
-                <h2 class="homeHeader__title">JUNTO</h2>
-                <h3 class="homeHeader__subtitle">A Movement For Authenticity</h3>
-                <p class="homeHeader__description">Junto is a new breed of social media founded
-                    to rebalance our relationship with technology. Our goal is to move beyond today's extractive paradigm through more human-centered design, distributed technology, and a nonprofit approach.
+                <h2 class="homeHeader__title">JUNTO is now Flux</h2>
+                <h3 class="homeHeader__subtitle">A Social Toolkit for the New Internet</h3>
+                <p class="homeHeader__description">Flux is a decentralized community platform that facilitates social coordination with greater privacy, collaboration, and flexibility. Leverage Flux for any and all your comunity needs, from group DMs to documentation, distributed governance, web3 integrations, and more.
                 </p>
                 <a class="removelink" href="https://www.indiegogo.com/projects/junto-a-new-breed-of-social-media--2#/" target="blank">
                     <div class="homeHeader__action">
-                        <img src="../../../assets/images/junto-home-2.0__flame.png" alt="" class="homeHeader__action--icon">
-                        <p class="homeHeader__action--text">GET EARLY ACCESS TO THE BETA</p>
+                        <!-- <img src="../../../assets/images/junto-home-2.0__flame.png" alt="" class="homeHeader__action--icon"> -->
+                        <p class="homeHeader__action--text">TRY FLUX</p>
                     </div>
                 </a>
             </div> 
-            <img class="homeHeader__mockups" src="../../../assets/images/junto-home-2.0__mockups.png" >  
+            <img class="homeHeader__mockups" src="../../../assets/images/junto-home-2.0__flux-mockup.png" >  
     </div>
 </template>
 
